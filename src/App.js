ac4bd6a39322b0d39a09d46ab4512df3b220f4cb
Sarwar-Ahmed <sarwarahmed109@gmail.com>
2020-08-27 04:05:47 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import CourseInfo from './Components/CourseInfo/CourseInfo';
 
 function App() {
   return (
-    <div >
-      <h1>Online Courses</h1>
+    <div className="container-fluid">
+      <h1 className="text-center bg-info p-3">Online Courses</h1>
+      <CourseInfo></CourseInfo>
     </div>
   );
 }
