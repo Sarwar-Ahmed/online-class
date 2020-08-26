cb19cb54812f392d0fb37db292800a238ec8b6fa
Sarwar-Ahmed <sarwarahmed109@gmail.com>
2020-08-27 01:07:13 +0600

    files added

---
+++ src/fakeData/fakeData.js
@@ -0,0 +1,95 @@
+var fakeData =
+    [
+        {
+            "id":0,
+            "title": "Complete Linux Training Course to Get Your Dream IT Job 2020",
+            "author": "Imran Afzal",
+            "price":9.99
+        },
+        {
+            "id":1,
+            "title": "Complete Linux Training Course to Get Your Dream IT Job 2020",
+            "author": "Imran Afzal",
+            "price":9.99
+        },
+        {
+            "id":2,
+            "title": "Complete Linux Training Course to Get Your Dream IT Job 2020",
+            "author": "Imran Afzal",
+            "price":9.99
+        },
+        {
+            "id":3,
+            "title": "Complete Linux Training Course to Get Your Dream IT Job 2020",
+            "author": "Imran Afzal",
+            "price":9.99
+        },
+        {
+            "id":4,
+            "title": "Complete Linux Training Course to Get Your Dream IT Job 2020",
+            "author": "Imran Afzal",
+            "price":9.99
+        },
+        {
+            "id":5,
+            "title": "Complete Linux Training Course to Get Your Dream IT Job 2020",
+            "author": "Imran Afzal",
+            "price":9.99
+        },
+        {
+            "id":6,
+            "title": "Complete Linux Training Course to Get Your Dream IT Job 2020",
+            "author": "Imran Afzal",
+            "price":9.99
+        },
+        {
+            "id":7,
+            "title": "Complete Linux Training Course to Get Your Dream IT Job 2020",
+            "author": "Imran Afzal",
+            "price":9.99
+        },
+        {
+            "id":8,
+            "title": "Complete Linux Training Course to Get Your Dream IT Job 2020",
+            "author": "Imran Afzal",
+            "price":9.99
+        },
+        {
+            "id":9,
+            "title": "Complete Linux Training Course to Get Your Dream IT Job 2020",
+            "author": "Imran Afzal",
+            "price":9.99
+        },
+        {
+            "id":10,
+            "title": "Complete Linux Training Course to Get Your Dream IT Job 2020",
+            "author": "Imran Afzal",
+            "price":9.99
+        },
+        {
+            "id":11,
+            "title": "Complete Linux Training Course to Get Your Dream IT Job 2020",
+            "author": "Imran Afzal",
+            "price":9.99
+        },
+        {
+            "id":12,
+            "title": "Complete Linux Training Course to Get Your Dream IT Job 2020",
+            "author": "Imran Afzal",
+            "price":9.99
+        },
+        {
+            "id":13,
+            "title": "Complete Linux Training Course to Get Your Dream IT Job 2020",
+            "author": "Imran Afzal",
+            "price":9.99
+        },
+        {
+            "id":14,
+            "title": "Complete Linux Training Course to Get Your Dream IT Job 2020",
+            "author": "Imran Afzal",
+            "price":9.99
+        }
+    ];
+
+export default fakeData;
